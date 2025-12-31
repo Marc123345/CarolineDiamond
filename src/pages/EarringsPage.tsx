7914ea79bd0094e307e445bf03776ff0406054f3
Marc123345 <84985026+Marc123345@users.noreply.github.com>
2025-12-31 02:11:09 +0200

    Updated EarringsPage.tsx

--- src/pages/EarringsPage.tsx
+++ src/pages/EarringsPage.tsx
@@ -1,17 +1,6 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { ShopPage } from './ShopPage';
 
-interface EarringsPageProps {
-  onNavigate: (page: string) => void;
-}
-
-export const EarringsPage: React.FC<EarringsPageProps> = ({ onNavigate }) => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate('/shop?category=earrings', { replace: true });
-  }, [navigate]);
-
-  return <ShopPage onNavigate={onNavigate} initialCategory="Earrings" />;
-};
+export const EarringsPage = ({ onNavigate }: { onNavigate: (path: string) => void }) => (
+  <ShopPage onNavigate={onNavigate} initialCategory="Earrings" />
+);
