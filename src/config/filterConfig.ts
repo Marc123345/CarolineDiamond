c9a5eea7f5940f7ea3ea0a4589f3b28456fdaf8e
Marc123345 <84985026+Marc123345@users.noreply.github.com>
2025-12-31 02:15:46 +0200

    Updated filterConfig.ts

--- src/config/filterConfig.ts
+++ src/config/filterConfig.ts
@@ -116,17 +116,17 @@ export const CARAT_WEIGHTS = [
 
 // Diamond Clarity Grades
 export const CLARITY_GRADES = [
-  'FL',  // Flawless
-  'IF',  // Internally Flawless
+  'FL',   // Flawless
+  'IF',   // Internally Flawless
   'VVS1', // Very Very Slightly Included 1
   'VVS2', // Very Very Slightly Included 2
-  'VS1', // Very Slightly Included 1
-  'VS2', // Very Slightly Included 2
-  'SI1', // Slightly Included 1
-  'SI2', // Slightly Included 2
-  'I1',  // Included 1
-  'I2',  // Included 2
-  'I3'   // Included 3
+  'VS1',  // Very Slightly Included 1
+  'VS2',  // Very Slightly Included 2
+  'SI1',  // Slightly Included 1
+  'SI2',  // Slightly Included 2
+  'I1',   // Included 1
+  'I2',   // Included 2
+  'I3'    // Included 3
 ] as const;
 
 // Common clarity filters (most searched)
